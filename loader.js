09034ee552511cd4bffc878783bf8cc1002f0ab8
Vylonium <vylonium@gmail.com>
2026-07-04 08:07:59 +0700

    fix(loader): daftarkan data_vietnam.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 // Loader: muat semua file data negara secara berurutan, lalu app.js
 (function() {
-  var files = ["data_indonesia.js", "data_jepang.js", "data_mesir.js", "data_romawi.js", "data_tiongkok.js", "data_inggris.js", "data_prancis.js", "data_jerman.js", "data_spanyol.js", "data_portugal.js", "data_korea.js", "data_rusia.js", "data_afsel.js", "data_argentina.js", "data_belanda.js", "data_italia.js", "data_polandia.js", "data_malaysia.js", "data_thailand.js", "data_filipina.js", "data_turki.js"];
+  var files = ["data_indonesia.js", "data_jepang.js", "data_mesir.js", "data_romawi.js", "data_tiongkok.js", "data_inggris.js", "data_prancis.js", "data_jerman.js", "data_spanyol.js", "data_portugal.js", "data_korea.js", "data_rusia.js", "data_afsel.js", "data_argentina.js", "data_belanda.js", "data_italia.js", "data_polandia.js", "data_malaysia.js", "data_thailand.js", "data_filipina.js", "data_turki.js", "data_vietnam.js"];
   var i = 0;
   function loadNext() {
     if (i >= files.length) { loadApp(); return; }
